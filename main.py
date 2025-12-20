11ca428ff1db40ac8115c6e5998643e56d7f34bc
Sena <senalikescake@gmail.com>
2025-12-20 16:28:47 +0000

    Implemented add instruction and support for R-Type instructions

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ def GetSpecificBits(num, start, span):
     temp = int(temp, 2) 
     return bin((num >> start) & temp)
 
-def Registers(read1, read2, writeReg, writeData = None):
-
-    return ALU(read1, read2)
-
 def RegWrite(instruction):
+    if GetSpecificBits(instruction, 0, 7) == '0b110011':
+        return True
+    else:
+        return False
 
-    return None
-
-def ALU(read1, read2, aluControl):
-    def ADD(num1, num2):
-        return num1 + num2
-    return None
+def ALU(read1, read2, aluControl = None):
+    read1 = int(read1, 2)
+    read2 = int(read2, 2)
+    def Add(num1, num2):
+        return bin(num1 + num2)
+    return Add(read1, read2)
 
 ### Initialization
 
@@ -30,24 +30,36 @@ for i in range (255):
     instructionMemory.append(0b0)
 
 # TODO Read Files For Instructions 
-instructionMemory[0] = 0b00000000100010000001000110011
+instructionMemory[0] = 0b00000000010000100000001000110011
 
 ## Initialize Registers
 registers = []
 for i in range(32):
     registers.append(0)
+registers[4] = bin(4)
 
 ### Main Loop
 # TODO Figure Out Exit Condition
 while pc == 0:
     # Read Instruction
     instruction = instructionMemory[pc]
+    
+    # Registers Variables
+    read1 = GetSpecificBits(instruction, 15, 5)
+    read2 = GetSpecificBits(instruction, 20, 5)
+    writeReg = GetSpecificBits(instruction, 7, 5)
+    writeData = (ALU(read1, read2))
 
-    Registers(GetSpecificBits(instruction, 15, 5), GetSpecificBits(instruction, 20, 5), GetSpecificBits(instruction, 7, 5))
-
-
+    ### End of Cycle
+    # Update Registers if RegWrite
+    if RegWrite(instruction):
+        registers[int(f"{writeReg}", 2)] = writeData
     # Update Program Counter (End Of Cycle)
     pc += 4
     
 
-
+print(f"read1: {read1}")
+print(f"read2: {read2}")
+print(f"writeReg: {writeReg}")
+print(f"writeData: {writeData}")
+print(registers)
